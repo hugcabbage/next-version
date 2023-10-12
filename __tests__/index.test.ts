d569e62f590487e52c7e59f30fcafd63fe1b3760
hugcabbage <77980779+hugcabbage@users.noreply.github.com>
2023-10-13 02:07:11 +0800

    更新

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -5,16 +5,13 @@ import * as fs from 'fs'
 import * as path from 'path'
 import { nextVersion } from './main.test'
 
-const repo_path = '.'
-process.chdir(repo_path)
-
-// 读取文件内容
 const filePath = path.join(__dirname, 'tags.txt')
 const text = fs.readFileSync(filePath, 'utf8')
 const text_list = text.trim().split('\n')
 for (const x of text_list) {
   console.log(x.split('/').slice(-1)[0])
 }
+
 console.log('-----------------------')
 console.log(nextVersion('v', 1, text))
 console.log(nextVersion('v', 2, text))
